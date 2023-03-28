b79154ac40b0f91f2f548f6dee0f8a0cd1c8ed66
Dani Coleman <danicoleman00@gmail.com>
2023-03-28 14:38:26 -0600

    finished

--- lessons/enumerables/practice/find.rb
+++ lessons/enumerables/practice/find.rb
@@ -3,88 +3,93 @@
 
 
 def find_first_seven_letter_word # this one is done for you
-    words = ["capricious", "berry", "unicorn", "bag", "apple", "festering", "pretzel", "pencil"]
+  words = ["capricious", "berry", "unicorn", "bag", "apple", "festering", "pretzel", "pencil"]
 
-    words.find do |word|
-      word.length == 7
-    end
-
-    # expected return value is "unicorn"
+  words.find do |word|
+    word.length == 7
+  end
+  # expected return value is "unicorn"
 end
 
 def no_waldo
-    words = ["scarf", "sandcastle", "flag", "pretzel", "crow", "key"]
-    found = words.find do |word|
-
-      # Your code goes here
-
-    end
-
-    # expected return value is nil
+  words = ["scarf", "sandcastle", "flag", "pretzel", "crow", "key"]
+  found = words.find do |word|
+  # Your code goes here
+    word.include?('waldo')
+  end
+  # expected return value is nil
 end
 
 def find_waldo
-    words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
-
-    # Your code goes here
-
-    # expected return value is "waldo"
+  words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
+  # Your code goes here
+  words.find do |word|
+    word == 'waldo'
+  end
+  # expected return value is "waldo"
 end
 
 def cannot_find_3_letter_words
-    words = ["piglet", "porridge", "bear", "blueberry"]
-
-    # Your code goes here
-
-    # expected return value is nil
+  words = ["piglet", "porridge", "bear", "blueberry"]
+  # Your code goes here
+  words.find do |word|
+    word.length == 3
+  end
+  # expected return value is nil
 end
 
 def find_13
-    numbers = [2, 13, 19, 8, 3, 27]
-
-    # Your code goes here
-
-    # expected return value is 13
+  numbers = [2, 13, 19, 8, 3, 27]
+  # Your code goes here
+  numbers.find do |number|
+    number == 13
+  end
+  # expected return value is 13
 end
 
 def find_first_even_number
-    numbers = [3, 7, 13, 11, 10, 2, 17]
-
-    # Your code goes here
-
-    # expected return value is 10
+  numbers = [3, 7, 13, 11, 10, 2, 17]
+  # Your code goes here
+  numbers.find do |number|
+    number.even?
+  end
+  # expected return value is 10
 end
 
 def find_first_multiple_of_3
-    numbers = [2, 8, 9, 27, 24, 5]
-
-    # Your code goes here
-
-    # expected return value is 9
+  numbers = [2, 8, 9, 27, 24, 5]
+  # Your code goes here
+  numbers.find do |number|
+    number % 3 == 0
+  end
+  # expected return value is 9
 end
 
 def find_first_word_starting_with_q
-    words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
-
-    # Your code goes here
-
-    # expected return value is "quill"
+  words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
+  # Your code goes here
+  words.find do |word|
+    word.include?('q')
+  end
+  # expected return value is "quill"
 end
 
 def find_first_word_ending_with_er
-    words = ["biggest", "pour", "blight", "finger", "pie", "border"]
-
-    # Your code goes here
-
-    # expected return value is "finger"
+  words = ["biggest", "pour", "blight", "finger", "pie", "border"]
+  # Your code goes here
+  words.find do |word|
+    word.include?('er')
+  end
+  # expected return value is "finger"
 end
 
 def find_first_number_greater_than_20
-    numbers = [1, 8, 19, 21, 29, 31, 34]
-
-    # Your code goes here
-
-    # expected return value is 21
+  numbers = [1, 8, 19, 21, 29, 31, 34]
+  # Your code goes here
+  numbers.find do |number|
+    number > 20
+  end
+  # expected return value is 21
 end
 
 # p find_first_seven_letter_word
