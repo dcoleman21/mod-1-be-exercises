cd144dceb446c104d31f5949c00647dff58bead1
Dani Coleman <danicoleman00@gmail.com>
2023-03-28 14:38:55 -0600

    finished

--- lessons/enumerables/practice/map.rb
+++ lessons/enumerables/practice/map.rb
@@ -3,67 +3,75 @@
 
 
 def capitalize
-    names = ["alice", "bob", "charlie"]
-
-    # Your code goes here
-
-    # expected return value is ["Alice", "Bob", "Charlie"]
+  names = ["alice", "bob", "charlie"]
+  # Your code goes here
+  names.map do |name|
+      name.capitalize
+  end
+  # expected return value is ["Alice", "Bob", "Charlie"]
 end
 
 def doubles
-    numbers = [1, 2, 3, 4, 5]
-
-    # Your code goes here
-
-    # expected return value is [2, 4, 6, 8, 10]
+  numbers = [1, 2, 3, 4, 5]
+  # Your code goes here
+  numbers.map do |number|
+    number * 2
+  end
+  # expected return value is [2, 4, 6, 8, 10]
 end
 
 def squares
-    numbers = [1, 2, 3, 4, 5]
-
-    # Your code goes here
-
-    # expected return value is [1, 4, 9, 16, 25]
+  numbers = [1, 2, 3, 4, 5]
+  # Your code goes here
+  numbers.map do |number|
+    number ** 2
+  end
+  # expected return value is [1, 4, 9, 16, 25]
 end
 
 def lengths
-    names = ["alice", "bob", "charlie", "david", "eve"]
-
-    # Your code goes here
-
-    # expected return value is [5, 3, 7, 5, 3]
+  names = ["alice", "bob", "charlie", "david", "eve"]
+  # Your code goes here
+  names.map do |name|
+    name.length
+  end
+  # expected return value is [5, 3, 7, 5, 3]
 end
 
 def normalize_zip_codes
-    numbers = [234, 10, 9119, 38881]
-
-    # Your code goes here
-
-    # expected return value is ["00234", "00010", "09119", "38881"]
+  numbers = [234, 10, 9119, 38881]
+  # Your code goes here
+  numbers.map do |number|
+   number.to_s.rjust(5, '0')
+  end
+  # expected return value is ["00234", "00010", "09119", "38881"]
 end
 
 def backwards
-    names = ["alice", "bob", "charlie", "david", "eve"]
-
-    # Your code goes here
-
-    # expected return value is ["ecila", "bob", "eilrahc", "divad", "eve"]
+  names = ["alice", "bob", "charlie", "david", "eve"]
+  # Your code goes here
+  names.map do |name|
+    name.reverse
+  end
+  # expected return value is ["ecila", "bob", "eilrahc", "divad", "eve"]
 end
 
 def words_with_no_vowels
-    words = ["green", "sheep", "travel", "least", "boat"]
-
-    # Your code goes here
-
-    # expected return value is ["grn", "shp", "trvl", "lst", "bt"]
+  words = ["green", "sheep", "travel", "least", "boat"]
+  # Your code goes here
+  words.map do |word|
+    word.delete('a,e,i,o,u')
+  end
+  # expected return value is ["grn", "shp", "trvl", "lst", "bt"]
 end
 
 def trim_last_letter
-    animals = ["dog", "cat", "mouse", "frog", "platypus"]
-
-    # Your code goes here
-
-    # expected return value is ["do", "ca", "mous", "fro", "platypu"]
+  animals = ["dog", "cat", "mouse", "frog", "platypus"]
+  # Your code goes here
+  animals.map do |animal|
+    animal.chop
+  end
+  # expected return value is ["do", "ca", "mous", "fro", "platypu"]
 end
 
 
